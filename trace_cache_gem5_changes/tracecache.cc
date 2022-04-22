0ff25f5dceedda0f1630b79b7ba0166baa279324
Hailey Johnson <hljohnson22@cs.wisc.edu>
2022-04-21 22:20:09 -0500

    Cleaned spacing, got building
    It now builds. No run testing done.
    Cleaned spacing.
    Moved constructor over to .cc file so it was easier to edit everything.
    Fixed cpp problems (my bad)
    Made SimObject

--- trace_cache_gem5_changes/tracecache.cc
+++ trace_cache_gem5_changes/tracecache.cc
@@ -1,147 +1,177 @@
 #include "tracecache.hh"
 using namespace gem5;
-traceCache trace;
 
-// represents one line in the trace cache
+traceCache::traceCache(const traceCacheParams &params, int numSets, int assoc, int numInsns, int numBBs) :
+SimObject(params)
+{
+    // create an array of lines
+    line = new tcLine[assoc*numSets];
+    // set tc parameter
+    numSets = numSets;
+    assoc = assoc;
+    // set tc fields for describing trace cache
+    size = assoc*numSets;
+    maxNumInsns = numInsns;
+    maxNumBBs = numBBs;
+    // set tc fields for building a trace
+    buildingTrace = 0;
+    buildLineIndex = 0;
+    buildLine = new tcLine();
+    // set tc fields for stats tracking
+    globalHitCount = 0;
+    globalMissCount = 0;
+}
 
-
-// represents the trace cache as an array of tcLine objects
-// This trace cache is currently built to support one branch instruction per
-// trace cache line.
- 
+traceCache::traceCache(const traceCacheParams &params) :
+SimObject(params)
+{
+    // create an array of lines
+    line = new tcLine[1*64];
+    // set tc parameter
+    numSets = 64;
+    assoc = 1;
+    // set tc fields for describing trace cache
+    size = 1*64;
+    maxNumInsns = 16;
+    maxNumBBs = 1;
+    // set tc fields for building a trace
+    buildingTrace = 0;
+    buildLineIndex = 0;
+    buildLine = new tcLine();
+    // set tc fields for stats tracking
+    globalHitCount = 0;
+    globalMissCount = 0;
+}
 
 // to be ran every instruction fetch
 void traceCache::tcInsnFetch(int fetchAddr, int isCondBranch, int branchPred){
-	traceCache trace;
-
-        int traceHit = 0;
-        // conditional branch instructions mark the beginning of a new trace
-        if(isCondBranch){
-            // complete the trace currently being built
-            if(trace.buildingTrace){
-                trace.completeTrace();
-            }
-
-            // look for the current address and prediction in the trace cache
-            traceHit = trace.searchTraceCache(fetchAddr, branchPred);
-
-        }else{
-            if(trace.buildingTrace){
-                // if there is a trace currently being built, add this instruction
-                trace.buildTrace(fetchAddr, branchPred);
-            } // otherwise, do nothing
-        }
-    }
-
-// returns 1 if hit, 0 if miss
-    int traceCache::searchTraceCache(int fetchAddr, int branchPred){
-        // get the index bits from the fetch address
-        unsigned int mask = 0;
-        int index = 0;
-        int lowerSearchBound = 0;
-        int upperSearchBound = 0;
-        int numIndexBits = log2(trace.numSets);
-        int hit = 0;
-        mask = (1 << numIndexBits) - 1;
-        index = fetchAddr & mask;
-
-        // find bounds of tc lines to access
-        lowerSearchBound = index * trace.assoc;
-        upperSearchBound = lowerSearchBound + trace.assoc;
-
-        // search all lines in appropriate set for hit
-        for(int i = lowerSearchBound; i < upperSearchBound; i++ ){
-            hit = searchTraceLine(fetchAddr, branchPred, i);
-            if(hit){ // if there is a hit, exit early
-                // log the hit statistics
-                trace.logHitStats(fetchAddr, branchPred, i);
-                return 1;
-            }
-        }
-
-        // if there is no hit - we have a trace cache miss
-        trace.logMissStats(fetchAddr, branchPred);
-
-        // on a miss, we begin building a new trace
-        // first, we figure out in which line the new trace should reside
-        trace.buildLineIndex = trace.selectBuildLineIndex(lowerSearchBound, upperSearchBound);
-        // then, we begin building the trace
-        trace.buildTrace(fetchAddr, branchPred);
-        return 0;
-    }
-
-    void traceCache::logHitStats(int fetchAddr, int branchPred, int i){
-        trace.globalHitCount++;
-    }
-
-    void traceCache::logMissStats(int fetchAddr, int branchPred){
-        trace.globalMissCount++;
-    }
-
-    void traceCache::buildTrace(int fetchAddr, int branchPred){
-        // if we are not currently building a trace, then this is the first
-        // instruction of the trace
-        if(trace.buildingTrace == 0){
-            // set up trace
-            trace.buildLine->tagAddr = fetchAddr; // save the PC
-            trace.buildLine->branchFlags = branchPred;
-            trace.buildLine->insnCount = 1;
-            trace.buildLine->BBCount = 1;
-            // tell system we are now building a trace
-            trace.buildingTrace = 1;
-        }
-        // if we are currently building a trace, then we need to add this insn to the trace
-        else{
-            // increment the instruction count of the line
-            trace.buildLine->insnCount++;
-        }
-
-        // check whether the trace has reached its max capacity
-        if(trace.buildLine->insnCount >= trace.maxNumInsns){
+int traceHit = 0;
+    // conditional branch instructions mark the beginning of a new trace
+    if(isCondBranch){
+        // complete the trace currently being built
+        if(buildingTrace){
             completeTrace();
         }
-    }
+        // look for the current address and prediction in the trace cache
+        traceHit = searchTraceCache(fetchAddr, branchPred);
 
-    int traceCache::searchTraceLine(int fetchAddr, int branchPred, int lineIndex){
-        // check for hit conditions
-        if((fetchAddr == trace.line[lineIndex].tagAddr)&
-           (branchPred == trace.line[lineIndex].branchFlags)&
-           (trace.line[lineIndex].valid == 1)){
-               return 1; // line hit
-        }else{
-            return 0; // line miss
+    }else{
+        if(buildingTrace){
+            // if there is a trace currently being built, add this instruction
+            buildTrace(fetchAddr, branchPred);
+        } // otherwise, do nothing
+    }
+}
+
+// returns 1 if hit, 0 if miss
+int traceCache::searchTraceCache(int fetchAddr, int branchPred){
+    // get the index bits from the fetch address
+    unsigned int mask = 0;
+    int index = 0;
+    int lowerSearchBound = 0;
+    int upperSearchBound = 0;
+    int numIndexBits = log2(numSets);
+    int hit = 0;
+    mask = (1 << numIndexBits) - 1;
+    index = fetchAddr & mask;
+
+    // find bounds of tc lines to access
+    lowerSearchBound = index * assoc;
+    upperSearchBound = lowerSearchBound + assoc;
+
+    // search all lines in appropriate set for hit
+    for(int i = lowerSearchBound; i < upperSearchBound; i++ ){
+        hit = searchTraceLine(fetchAddr, branchPred, i);
+        if(hit){ // if there is a hit, exit early
+            // log the hit statistics
+            logHitStats(fetchAddr, branchPred, i);
+            return 1;
         }
     }
 
-    int traceCache::selectBuildLineIndex(int lowerSearchBound, int upperSearchBound){
-        // first, see if there are any lines which are invalid
-        for(int i = lowerSearchBound; i < upperSearchBound; i++){
-            if(trace.line[i].valid == 0){ // found an invalid line
-                return i;
-            }
+    // if there is no hit - we have a trace cache miss
+    logMissStats(fetchAddr, branchPred);
+
+    // on a miss, we begin building a new trace
+    // first, we figure out in which line the new trace should reside
+    buildLineIndex = selectBuildLineIndex(lowerSearchBound, upperSearchBound);
+    // then, we begin building the trace
+    buildTrace(fetchAddr, branchPred);
+    return 0;
+}
+
+
+void traceCache::logHitStats(int fetchAddr, int branchPred, int i){
+    globalHitCount++;
+}
+
+void traceCache::logMissStats(int fetchAddr, int branchPred){
+    globalMissCount++;
+}
+
+
+void traceCache::buildTrace(int fetchAddr, int branchPred){
+    // if we are not currently building a trace, then this is the first
+    // instruction of the trace
+    if(buildingTrace == 0){
+        // set up trace
+        buildLine->tagAddr = fetchAddr; // save the PC
+        buildLine->branchFlags = branchPred;
+        buildLine->insnCount = 1;
+        buildLine->BBCount = 1;
+        // tell system we are now building a trace
+        buildingTrace = 1;
+    }
+    // if we are currently building a trace, then we need to add this insn to the trace
+    else{
+        // increment the instruction count of the line
+        buildLine->insnCount++;
+    }
+
+    // check whether the trace has reached its max capacity
+    if(buildLine->insnCount >= maxNumInsns){
+        completeTrace();
+    }
+}
+
+int traceCache::searchTraceLine(int fetchAddr, int branchPred, int lineIndex){
+    // check for hit conditions
+    if((fetchAddr == line[lineIndex].tagAddr)&
+      (branchPred == line[lineIndex].branchFlags)&
+      (line[lineIndex].valid == 1)){
+        return 1; // line hit
+    }else{
+        return 0; // line miss
+    }
+}
+
+
+int traceCache::selectBuildLineIndex(int lowerSearchBound, int upperSearchBound){
+    // first, see if there are any lines which are invalid
+    for(int i = lowerSearchBound; i < upperSearchBound; i++){
+        if(line[i].valid == 0){ // found an invalid line
+            return i;
         }
-        // if there are no invalid cache lines, randomly pick a line to evict
-        return lowerSearchBound + (rand() % upperSearchBound);
     }
+    // if there are no invalid cache lines, randomly pick a line to evict
+    return lowerSearchBound + (rand() % upperSearchBound);
+}
 
-    void traceCache::completeTrace(){
-        // copy all buildLine values to the correct line in the tc
-        trace.line[trace.buildLineIndex].tagAddr = trace.buildLine->tagAddr;
-        trace.line[trace.buildLineIndex].branchFlags = trace.buildLine->branchFlags;
-        trace.line[trace.buildLineIndex].insnCount = trace.buildLine->insnCount;
-        trace.line[trace.buildLineIndex].BBCount = trace.buildLine->BBCount;
-        trace.line[trace.buildLineIndex].valid = 1;
+void traceCache::completeTrace(){
+    // copy all buildLine values to the correct line in the tc
+    line[buildLineIndex].tagAddr = buildLine->tagAddr;
+    line[buildLineIndex].branchFlags = buildLine->branchFlags;
+    line[buildLineIndex].insnCount = buildLine->insnCount;
+    line[buildLineIndex].BBCount = buildLine->BBCount;
+    line[buildLineIndex].valid = 1;
 
-        // clear out all buildLine values
-        trace.buildLine->tagAddr = 9;
-        trace.buildLine->branchFlags = 0;
-        trace.buildLine->insnCount = 0;
-        trace.buildLine->BBCount = 0;
+    // clear out all buildLine values
+    buildLine->tagAddr = 9;
+    buildLine->branchFlags = 0;
+    buildLine->insnCount = 0;
+    buildLine->BBCount = 0;
 
-        // no longer building a trace
-        trace.buildingTrace = 0;
-        trace.buildLineIndex = 0;
-    }
-
-
-
+    // no longer building a trace
+    buildingTrace = 0;
+    buildLineIndex = 0;
+}
--- trace_cache_gem5_changes/tracecache.hh
+++ trace_cache_gem5_changes/tracecache.hh
@@ -1,4 +1,3 @@
-
 #ifndef __TRACE_CACHE_HH__
 #define __TRACE_CACHE_HH__
 
@@ -6,123 +5,85 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
-#include "params/tracecache.hh"
+#include "params/traceCache.hh"
 #include "sim/sim_object.hh"
 
 namespace gem5{
+    // represents one line in the trace cache
+    class tcLine
+    {
+        public:
+            // fields for accessing the tc line
+            int   tagAddr;
+            int   valid;
+            int   branchFlags;
 
-// represents one line in the trace cache
-class tcLine
-{
-  public:
-    // fields for accessing the tc line
-    int   tagAddr;
-    int   valid;
-    int   branchFlags;
+            int insnCount; // number of instructions in this line
+            int BBCount; // number of basic blocks in this line
 
-    int insnCount; // number of instructions in this line
-    int BBCount; // number of basic blocks in this line
+            // Constructor
+            tcLine() {
+                // set fields for accessing a tc line
+                this->tagAddr = 0;
+                this->valid = 0;
+                this->branchFlags = 0;
+                // set fields for building a trace
+                this->insnCount = 0;
+                this->BBCount = 0;
+            }
+    };
 
-    // Constructor
-    tcLine() {
-      // set fields for accessing a tc line
-      this->tagAddr = 0;
-      this->valid = 0;
-      this->branchFlags = 0;
-      // set fields for building a trace
-      this->insnCount = 0;
-      this->BBCount = 0;
-    }
-};
+    // represents the trace cache as an array of tcLine objects
+    // This trace cache is currently built to support one branch instruction per
+    // trace cache line.
+    class traceCache : public SimObject
+    {
+        public:
+            // parameters
+            int numSets; // number of sets in the cache
+            int assoc; // associativity of the cache
 
-// represents the trace cache as an array of tcLine objects
-// This trace cache is currently built to support one branch instruction per
-// trace cache line.
-class traceCache : public SimObject
-{
-  public:
-    // parameters
-    int numSets; // number of sets in the cache
-    int assoc; // associativity of the cache
+            // fields describing the trace cache
+            tcLine* line;
+            int    size; // size of the cache in terms of lines
+            int    maxNumInsns; // max number of insns in one cache line
+            int    maxNumBBs = 1; // max number of basic blocks in one cache line
 
-    // fields describing the trace cache
-    tcLine* line;
-    int    size; // size of the cache in terms of lines
-    int    maxNumInsns; // max number of insns in one cache line
-    int    maxNumBBs = 1; // max number of basic blocks in one cache line
+            // fields for building a trace in the trace cache
+            int buildingTrace; // 1 = currently building a trace, 0 = otherwise
+            int buildLineIndex; // hold line # is trace cache that trace
+            // is being built for
+            tcLine* buildLine; // used to hold stats on tc line currently being built
 
-    // fields for building a trace in the trace cache
-    int buildingTrace; // 1 = currently building a trace, 0 = otherwise
-    int buildLineIndex; // hold line # is trace cache that trace
-                        // is being built for
-    tcLine* buildLine; // used to hold stats on tc line currently being built
+            // Stats tracking
+            int globalHitCount;
+            int globalMissCount;
 
-    // Stats tracking
-    int globalHitCount;
-    int globalMissCount;
+            //default consrtuctor
+            traceCache(const traceCacheParams &params);
 
+            // Constructor
+            traceCache(const traceCacheParams &params, int numSets, int assoc, int numInsns, int numBBs);
 
-    traceCache() {
-        // create an array of lines
-        line = new tcLine[1*64];
-        // set tc parameter
-        this->numSets = 64;
-        this->assoc = 1;
-        // set tc fields for describing trace cache
-        this->size = 1*64;
-        this->maxNumInsns = 16;
-        this->maxNumBBs = 1;
-        // set tc fields for building a trace
-        this->buildingTrace = 0;
-        this->buildLineIndex = 0;
-        this->buildLine = new tcLine();
-        // set tc fields for stats tracking
-        this->globalHitCount = 0;
-        this->globalMissCount = 0;
-    }
+            // to be ran every instruction fetch
+            void tcInsnFetch(int fetchAddr, int isCondBranch, int branchPred);
 
+            // returns 1 if hit, 0 if miss
+            int searchTraceCache(int fetchAddr, int branchPred);
 
-    // Constructor
-    traceCache(int numSets, int assoc, int numInsns, int numBBs) {
-        // create an array of lines
-        line = new tcLine[assoc*numSets];
-        // set tc parameter
-        this->numSets = numSets;
-        this->assoc = assoc;
-        // set tc fields for describing trace cache
-        this->size = assoc*numSets;
-        this->maxNumInsns = numInsns;
-        this->maxNumBBs = numBBs;
-        // set tc fields for building a trace
-        this->buildingTrace = 0;
-        this->buildLineIndex = 0;
-        this->buildLine = new tcLine();
-        // set tc fields for stats tracking
-        this->globalHitCount = 0;
-        this->globalMissCount = 0;
-    }
+            void logHitStats(int fetchAddr, int branchPred, int i);
 
-    // to be ran every instruction fetch
-    void tcInsnFetch(int fetchAddr, int isCondBranch, int branchPred);
+            void logMissStats(int fetchAddr, int branchPred);
 
+            void buildTrace(int fetchAddr, int branchPred);
 
-    // returns 1 if hit, 0 if miss
-    int searchTraceCache(int fetchAddr, int branchPred);
+            int searchTraceLine(int fetchAddr, int branchPred, int lineIndex);
 
-    void logHitStats(int fetchAddr, int branchPred, int i);
+            int selectBuildLineIndex(int lowerSearchBound, int upperSearchBound);
 
-    void logMissStats(int fetchAddr, int branchPred);
+            void completeTrace();
 
-    void buildTrace(int fetchAddr, int branchPred);
-
-    int searchTraceLine(int fetchAddr, int branchPred, int lineIndex);
-
-    int selectBuildLineIndex(int lowerSearchBound, int upperSearchBound);
-
-    void completeTrace();
-
-};
+    };
 } // for the namespace gem5
 
 #endif //_TRACE_CACHE_HH__
-
